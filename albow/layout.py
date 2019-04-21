ada02ffd8210b2a3663e3f2fb840c513ca6276fc
Humberto Sanchez II <Humberto.A.Sanchez.II@gmail.com>
2019-04-21 18:35:13 -0500

    Remove commented out code

--- albow/layout.py
+++ albow/layout.py
@@ -1,34 +1,18 @@
-#---------------------------------------------------------------------------
-#
-#   Albow - Layout widgets
-#
-#---------------------------------------------------------------------------
-
 
 from albow.layouts.RowOrColumn import RowOrColumn
 
-class Row(RowOrColumn):
 
-    # d          = (1, 0)
-    # axis       = 'h'
-    # minor_axis = 'h'
-    # longways   = 'width'
-    # crossways  = 'height'
-    # align_map = {
-    #     't': (0, 'topleft', 'topright'),
-    #     'c': (1, 'midleft', 'midright'),
-    #     'b': (2, 'bottomleft', 'bottomright'),
-    # }
+class Row(RowOrColumn):
 
     def __init__(self, items, width=None, **kwds):
         """
-        Row(items, align = alignment, spacing = 10, width = None, expand = None)
-        align = 't', 'c' or 'b'
+
+        :param items:
+        :param width:
+        :param kwds:
         """
-        # Python 3 update
-        #
-        # RowOrColumn.__init__(self, width, items, kwds)
-        self.d          = (1,0)
+
+        self.d          = (1, 0)
         self.minor_axis = 'h'
         self.axis       = 'h'
         self.longways   = 'width'
@@ -39,30 +23,18 @@ class Row(RowOrColumn):
                 'b': (2, 'bottomleft', 'bottomright'),
         }
 
-
         super().__init__(width, items, kwds)
+
 
 class Column(RowOrColumn):
 
-    # d          = (0, 1)
-    # axis       = 'v'
-    # minor_axis = 'w'
-    # longways   = 'height'
-    # crossways  = 'width'
-    # align_map = {
-    #     'l': (0, 'topleft',  'bottomleft'),
-    #     'c': (1, 'midtop',   'midbottom'),
-    #     'r': (2, 'topright', 'bottomright'),
-    # }
-
     def __init__(self, items, height=None, **kwds):
         """
-        Column(items, align = alignment, spacing = 10, height = None, expand = None)
-        align = 'l', 'c' or 'r'
+
+        :param items:
+        :param height:
+        :param kwds:
         """
-        # Python 3 update
-        #
-        # RowOrColumn.__init__(self, height, items, kwds)
 
         self.d          = (0, 1)
         self.minor_axis = 'w'
