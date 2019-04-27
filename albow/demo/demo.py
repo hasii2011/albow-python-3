f86b47d1180dd343cfe3c4e4e69eaa9493af042d
Humberto Sanchez II <Humberto.A.Sanchez.II@gmail.com>
2019-04-27 18:36:07 -0500

    More refactoring; move classes to files

--- albow/demo/demo.py
+++ albow/demo/demo.py
@@ -41,6 +41,7 @@ from albow.demo.screens.DemoPaletteViewScreen import DemoPaletteViewScreen
 from albow.demo.screens.DemoImageArrayScreen import DemoImageArrayScreen
 from albow.demo.screens.DemoAnimationScreen import DemoAnimationScreen
 from albow.demo.screens.DemoControlsScreen import DemoControlsScreen
+from albow.demo.screens.DemoTextFieldsScreen import DemoTextFieldsScreen
 
 # screen_size = (640, 480)
 screen_size = (480, 640)
@@ -108,61 +109,6 @@ class MenuScreen(Screen):
 
     def quit(self):
         sys.exit(0)
-
-
-class DemoTextFieldsScreen(Screen):
-    """
-    Text Field
-    """
-
-    def __init__(self, shell):
-        """
-
-        :param shell:
-        """
-        attrs = {'bg_color': Theme.WHITE}
-        #
-        # Python 3 update
-        #
-        # Screen.__init__(self, shell)
-        super().__init__(shell, **attrs)
-
-        self.fld1 = self.add_field("Name", 200)
-        self.fld2 = self.add_field("Race", 250)
-        btn = Button("OK", action=self.ok)
-        btn.rect.midtop = (320, 300)
-        self.add(btn)
-        out = Label("")
-        out.rect.width = 400
-        out.rect.topleft = (200, 350)
-        self.out = out
-        self.add(out)
-        btn = Button("Menu", action=self.go_back)
-        btn.rect.midtop = (320, 400)
-        self.add(btn)
-        self.fld1.focus()
-
-    def add_field(self, label, pos):
-        """
-
-        :param label:
-        :param pos:
-        :return:
-        """
-        lbl = Label(label)
-        lbl.rect.topleft = (200, pos)
-        self.add(lbl)
-        fld = TextField(150)
-        fld.rect.topleft = (250, pos)
-        fld.enter_action = self.ok
-        self.add(fld)
-        return fld
-
-    def ok(self):
-        self.out.text = "You are a %s called %s." % (self.fld2.text, self.fld1.text)
-
-    def go_back(self):
-        self.parent.show_menu()
 
 
 class DemoDialogScreen(Screen):
