8e21b5fd64064a1868b267cde5d5fd64b91f5c24
Humberto Sanchez II <Humberto.A.Sanchez.II@gmail.com>
2019-04-27 19:14:18 -0500

    More refactoring, I do not like magic values

--- albow/demo/demo.py
+++ albow/demo/demo.py
@@ -12,8 +12,10 @@ import logging.config
 from albow.demo.DemoShell import DemoShell
 # SCREEN_SIZE   = (640, 480)
 # DISPLAY_FLAGS = pygame.RESIZABLE
-SCREEN_SIZE = (480, 640)
-DISPLAY_FLAGS    = 0
+LOGGING_CONFIG_FILENAME = "logging.conf"
+DEMO_WINDOW_TITLE       = "Albow Demonstration"
+SCREEN_SIZE             = (480, 640)
+DISPLAY_FLAGS           = 0
 
 sys.path.insert(1, d(d(os.path.abspath(sys.argv[0]))))
 
@@ -21,9 +23,9 @@ sys.path.insert(1, d(d(os.path.abspath(sys.argv[0]))))
 def main():
 
     pygame.init()
-    pygame.display.set_caption("Albow Demonstration")
+    pygame.display.set_caption("%s" % DEMO_WINDOW_TITLE)
 
-    logging.config.fileConfig('logging.conf')
+    logging.config.fileConfig("%s" % LOGGING_CONFIG_FILENAME)
 
     logger  = logging.getLogger(__name__)
     display = pygame.display.set_mode(SCREEN_SIZE, DISPLAY_FLAGS)
