0ec03bcfb813db3d351be596111fa8f73c4055b4
Humberto Sanchez II <Humberto.A.Sanchez.II@gmail.com>
2019-05-02 11:03:11 -0500

    Correct name

--- albow/demo/screens/DemoMusicScreen.py
+++ albow/demo/screens/DemoMusicScreen.py
@@ -7,15 +7,17 @@ from albow.layout.Column import Column
 
 from albow.widgets.Button import Button
 
+from albow.dialog.DialogUtilities import alert
+
 from albow.demo.screens.BaseDemoScreen import BaseDemoScreen
 
 from albow.media.MusicOptionsDialog import MusicOptionsDialog
 from albow.media.PlayList import PlayList
 
 from albow.media.EnableMusicControl import change_playlist
+from albow.media.EnableMusicControl import set_music_enabled
 
 from albow.media.music import get_music
-
 
 class DemoMusicScreen(BaseDemoScreen):
 
@@ -35,10 +37,10 @@ class DemoMusicScreen(BaseDemoScreen):
             'font': self.smallButtonFont
         }
         launchMusicDialogButt: Button = Button(text="Options Dialog", action=self.testOptionsDialog, **attrs)
-        startDemoMusicButt:    Button = Button(text="Start Music",    action=self.testStartMusic,    **attrs)
+        loadDemoMusicButt:     Button = Button(text="Load Music",     action=self.testLoadMusic, **attrs)
 
         contents = Column([launchMusicDialogButt,
-                           startDemoMusicButt,
+                           loadDemoMusicButt,
                            self.backButton], **columnAttrs)
         self.add_centered(contents)
         self.backButton.focus()
@@ -49,15 +51,16 @@ class DemoMusicScreen(BaseDemoScreen):
 
         dialog.present()
 
-    def testStartMusic(self):
+    def testLoadMusic(self):
 
         path1 = get_music("ElecPiK04 75E-01.mp3")
         path2 = get_music("ElecPiK04 75E-02.mp3")
         path3 = get_music("ElecPiK04 75E-03.mp3")
         path4 = get_music("ElecPiK04 75E-04.mp3")
 
+        set_music_enabled(False)
         paths = {path1, path2, path3, path4}
         playList = PlayList(paths)
         change_playlist(new_playlist=playList)
 
-
+        alert("Music Loaded")
