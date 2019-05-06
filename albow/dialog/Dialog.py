55656d478b9f02b14d4b6eb94bb3fdbb2b5b4388
Humberto Sanchez II <Humberto.A.Sanchez.II@gmail.com>
2019-05-06 07:42:49 -0500

    Documentation update

--- albow/dialog/Dialog.py
+++ albow/dialog/Dialog.py
@@ -7,12 +7,32 @@ from albow.layout.Row import Row
 from albow.layout.Column import Column
 
 
-
 class Dialog(Modal, Widget):
+    """
+    The Dialog class provides a convenient container for implementing modal dialogs. Pressing Return or
+    Enter dismisses the dialog with the value True, and pressing Escape dismisses it with the value False.
 
+    See the present() and dismiss() methods of class Widget for showing and dismissing the dialog.
+    """
     click_outside_response = None
+    """
+    If this attribute is given a non-None value, then a mouse-down event outside the bounds of the dialog will 
+    cause it to be dismissed with the given value.
+    """
+    def __init__(self, client=None, responses=None, default=0, cancel=-1, **kwds):
+        """
 
-    def __init__(self, client = None, responses = None, default = 0, cancel = -1, **kwds):
+        Args:
+            client:    The widget the dialog is on top of
+
+            responses: A list of responses
+
+            default:   The index to the default response; Default is the first
+
+            cancel:    The index to the cancel response; Default is None
+
+            **kwds:
+        """
 
         Widget.__init__(self, **kwds)
         if client or responses:
@@ -24,26 +44,34 @@ class Dialog(Modal, Widget):
                 w1 = client.width
             if responses:
                 buttons = Row([
-                    Button(text, action = lambda t=text: self.dismiss(t))
-                    for text in responses], equalize = 'w')
+                    Button(text, action=lambda t=text: self.dismiss(t))
+                    for text in responses], equalize='w')
                 rows.append(buttons)
                 w2 = buttons.width
             if w1 < w2:
                 a = 'l'
             else:
                 a = 'r'
-            contents = Column(rows, align = a)
+            contents = Column(rows, align=a)
             m = self.margin
             contents.topleft = (m, m)
             self.add(contents)
             self.shrink_wrap()
+        #
+        # TODO  Appears to be some code for future use to provide other than True/False responses
+        #
         if responses and default is not None:
             self.enter_response = responses[default]
         if responses and cancel is not None:
             self.cancel_response = responses[cancel]
 
     def mouse_down(self, e):
-        if not e in self:
+        #
+        # PEP 8 update
+        # https://lintlyci.github.io/Flake8Rules/rules/E713.html
+        #
+        # if not e in self:
+        if e not in self:
             response = self.click_outside_response
             if response is not None:
                 self.dismiss(response)
--- albow/dialog/Modal.py
+++ albow/dialog/Modal.py
@@ -1,8 +1,17 @@
 
 class Modal:
-
+    """
+    Modal is a mixin class for use by widgets that define modal states. It provides default responses for the Enter
+    and Escape keys and methods that can be used as actions for OK and Cancel buttons.
+    """
     enter_response = True
+    """
+    Value with which to dismiss a modal dialog when the Return or Enter key is pressed.
+    """
     cancel_response = False
+    """
+    Value with which to dismiss a modal dialog when the Escape key is pressed.
+    """
 
     # These aren't used because subclasses of Modal seem to be using
     # Widget.dismiss()
