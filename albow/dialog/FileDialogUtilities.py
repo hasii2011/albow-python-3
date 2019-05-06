8a5ff9fbe70095d96f87e4fd72e4f77bec3ce62a
Humberto Sanchez II <Humberto.A.Sanchez.II@gmail.com>
2019-05-06 09:33:35 -0500

    Update the online documentation

--- albow/dialog/FileDialogUtilities.py
+++ albow/dialog/FileDialogUtilities.py
@@ -1,10 +1,5 @@
 
-"""
-    Albow file dialog utility methods
-"""
-
 import os
-
 
 from albow.dialog.FileOpenDialog import FileOpenDialog
 from albow.dialog.LookForFileDialog import LookForFileDialog
@@ -12,6 +7,26 @@ from albow.dialog.FileSaveDialog import FileSaveDialog
 
 
 def request_new_filename(prompt=None, suffix=None, extra_suffixes=None, directory=None, filename=None, pathname=None):
+    """
+    Presents a dialog for specifying a new file.
+
+    Args:
+        prompt: The prompt is displayed as a prompt to the user.
+
+        suffix: The suffix, if any, will be appended to the filename returned if necessary, and also
+                specifies which files can be chosen.
+
+        extra_suffixes: The extra_suffixes can be a list of additional suffixes of choosable files.
+
+        directory:  Specifies the starting directory
+
+        filename:   The initial contents of the filename box
+
+        pathname:   Alternatively the pathname can be used to specify both of these together
+
+    Returns:    The full pathname of the file chosen, or None if the dialog is cancelled.
+
+    """
     if pathname:
         directory, filename = os.path.split(pathname)
     if extra_suffixes:
@@ -33,10 +48,16 @@ def request_new_filename(prompt=None, suffix=None, extra_suffixes=None, director
 
 def request_old_filename(suffixes=None, directory=None):
     """
+    Presents a dialog for choosing an existing file.
 
-    :param suffixes:
-    :param directory:
-    :return:
+    Args:
+        suffixes: The suffixes is a list of filename extensions defining
+                  the files that can be chosen; if not specified, any file can be chosen.
+
+        directory: The directory specifies the directory
+                   in which to start browsing; if unspecified, the current working directory is used.
+
+    Returns:    Returns the full pathname of the file chosen, or None if the dialog is cancelled.
     """
     attrs = {'margin': 10}
 
@@ -50,6 +71,20 @@ def request_old_filename(suffixes=None, directory=None):
 
 
 def look_for_file_or_directory(target, prompt=None, directory=None):
+    """
+    This function is used to ask the user to locate a file or directory with a specific name
+    Any directory can be visited, but only files whose last pathname component matches the target are visible
+    and choosable.
+
+    Args:
+        target: The file/directory to look for
+
+        prompt: The prompt to display
+
+        directory:  Specifies the directory in which to start browsing.
+
+    Returns:        Returns the full pathname of the file or directory chosen, or None if the dialog is cancelled.
+    """
 
     dlog = LookForFileDialog(target=target, prompt=prompt)
     if directory:
