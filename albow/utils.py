99abbb00350203702a66271e485c98d6aedf2dc8
Humberto Sanchez II <Humberto.A.Sanchez.II@gmail.com>
2019-05-11 10:11:52 -0500

    PEP-8 fix tab indentation problems

--- albow/utils.py
+++ albow/utils.py
@@ -1,25 +1,27 @@
 from pygame import Surface
 from pygame import Rect
 
+
 # from pygame.locals import SRCALPHA
 
 
 def frame_rect(surface: Surface, color, rect: Rect, thick: int = 1):
-	"""
-	Draws a border with thickness thick just inside the specified rectangle.
+    """
+    Draws a border with thickness thick just inside the specified rectangle.
 
-	Args:
-		surface: Pygame surface
+    Args:
+        surface: Pygame surface
 
-		color:
-		rect: pygame rectangle
+        color:
+        rect: pygame rectangle
 
-		thick:
-	"""
-	surface.fill(color, (rect.left, rect.top, rect.width, thick))
-	surface.fill(color, (rect.left, rect.bottom - thick, rect.width, thick))
-	surface.fill(color, (rect.left, rect.top, thick, rect.height))
-	surface.fill(color, (rect.right - thick, rect.top, thick, rect.height))
+        thick:
+    """
+    surface.fill(color, (rect.left, rect.top, rect.width, thick))
+    surface.fill(color, (rect.left, rect.bottom - thick, rect.width, thick))
+    surface.fill(color, (rect.left, rect.top, thick, rect.height))
+    surface.fill(color, (rect.right - thick, rect.top, thick, rect.height))
+
 
 #
 # This method is not used;  Also, use old & outdated
@@ -42,75 +44,75 @@ def frame_rect(surface: Surface, color, rect: Rect, thick: int = 1):
 # 	surface.blit(buf, pos)
 
 
-def blit_in_rect(dst, src, frame, align = 'tl', margin = 0):
-	"""
-	Draws the image onto the surface within the rectangle frame, aligned according to align. The
-	alignment string may contain any meaningful combination of the letters
+def blit_in_rect(dst, src, frame, align='tl', margin=0):
+    """
+    Draws the image onto the surface within the rectangle frame, aligned according to align. The
+    alignment string may contain any meaningful combination of the letters
 
-	- _l_
-	- _r_
-	- _t_
-	- _b_
-	- _c_
+    - _l_
+    - _r_
+    - _t_
+    - _b_
+    - _c_
 
-	for _left_, _right_, _top_, _bottom_ and _center_.
+    for _left_, _right_, _top_, _bottom_ and _center_.
 
-	For example, _bl_ aligns the bottom left corners of the image and frame, and _cr_
-	aligns their center right points.  If a margin is specified, the frame is considered to be reduced in size by
-	that amount on all sides.
+    For example, _bl_ aligns the bottom left corners of the image and frame, and _cr_
+    aligns their center right points.  If a margin is specified, the frame is considered to be reduced in size by
+    that amount on all sides.
 
-	Args:
-		dst:  destination
+    Args:
+        dst:  destination
 
-		src:	source
+        src:	source
 
-		frame:	frame
+        frame:	frame
 
-		align:	alignment string
+        align:	alignment string
 
-		margin: margin to provide
-	"""
-	r = src.get_rect()
-	align_rect(r, frame, align, margin)
-	dst.blit(src, r)
+        margin: margin to provide
+    """
+    r = src.get_rect()
+    align_rect(r, frame, align, margin)
+    dst.blit(src, r)
 
 
-def align_rect(r, frame, align = 'tl', margin = 0):
-	"""
-	Modifies the rectangle rect so that it is aligned with the rectangle frame according to align
-	and margin. See `blit_in_rect`.
+def align_rect(r, frame, align='tl', margin=0):
+    """
+    Modifies the rectangle rect so that it is aligned with the rectangle frame according to align
+    and margin. See `blit_in_rect`.
 
-	Args:
-		r:
-		frame:
-		align:
-		margin:
-	"""
-	if 'l' in align:
-		r.left = frame.left + margin
-	elif 'r' in align:
-		r.right = frame.right - margin
-	else:
-		r.centerx = frame.centerx
-	if 't' in align:
-		r.top = frame.top + margin
-	elif 'b' in align:
-		r.bottom = frame.bottom - margin
-	else:
-		r.centery = frame.centery
+    Args:
+        r:
+        frame:
+        align:
+        margin:
+    """
+    if 'l' in align:
+        r.left = frame.left + margin
+    elif 'r' in align:
+        r.right = frame.right - margin
+    else:
+        r.centerx = frame.centerx
+    if 't' in align:
+        r.top = frame.top + margin
+    elif 'b' in align:
+        r.bottom = frame.bottom - margin
+    else:
+        r.centery = frame.centery
 
 
 def brighten(rgb, factor):
-	"""
-	Returns a new color obtained by multiplying each component of the color rgb by the given factor, which
-	should be a number in the range 0.0 to 1.0.
+    """
+    Returns a new color obtained by multiplying each component of the color rgb by the given factor, which
+    should be a number in the range 0.0 to 1.0.
 
-	Args:
-		rgb:	The color to brighten
+    Args:
+        rgb:	The color to brighten
 
-		factor:	Should be a number in the range 0.0 to 1.0.
+        factor:	Should be a number in the range 0.0 to 1.0.
 
-	Returns:
-		A new RGB value
-	"""
-	return [min(255, int(round(factor * c))) for c in rgb]
+    Returns:
+        A new RGB value
+    """
+    return [min(255, int(round(factor * c))) for c in rgb]
