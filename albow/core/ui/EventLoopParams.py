9959ca99259190076d92ea60c9ac64c786b269d5
Humberto Sanchez II <Humberto.A.Sanchez.II@gmail.com>
2019-10-01 16:25:27 -0500

    PEP-8 Somehow the formatting got off on this file

--- albow/core/ui/EventLoopParams.py
+++ albow/core/ui/EventLoopParams.py
@@ -1,12 +1,13 @@
 
 from dataclasses import dataclass
 
+
 @dataclass
 class EventLoopParams:
 
-    use_sleep: bool
-    relative_pause: bool
-    do_draw: bool
+    use_sleep:       bool
+    relative_pause:  bool
+    do_draw:         bool
     relative_warmup: int
     last_click_time: int
-    num_clicks: int
+    num_clicks:      int
